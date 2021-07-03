978964d0525d83a90f1806d3f6eee8a8ce0abce0
ambrosy-eric <14945937+ambrosy-eric@users.noreply.github.com>
2021-07-03 11:05:25 -0500

    add some exception handling to the nato tool

--- day-26/main.py
+++ day-26/main.py
@@ -3,9 +3,18 @@ import pandas as pd
 def main():
     data = pd.read_csv('data/nato_phonetic_alphabet.csv')
     nato_dict = {row.letter:row.code for (index, row) in data.iterrows()}
-    word = input("Enter the word you would like the NATO Phonetic Alphabet for: ").upper()
-    result = [nato_dict[letter] for letter in word]
-    print(result)
+    
+    def _nato_alphabet():
+        word = input("Enter the word you would like the NATO Phonetic Alphabet for: ").upper()
+        try:
+            result = [nato_dict[letter] for letter in word]
+        except KeyError:
+            print('Only letters are valid')
+            _nato_alphabet()
+        else:
+            print(result)
+    
+    _nato_alphabet()
 
 
 if __name__ == '__main__':
